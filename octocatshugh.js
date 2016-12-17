216788ae5f9e71d977d711601fdf899c8504392f
hugh88 <7hgallagly@stpatrick.org>
2016-12-16 19:52:19 -0600

    fixed it
    i messed up the order and forgot to save it as .js

---
+++ octocatshugh.js
@@ -0,0 +1,17 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+function Octocat(n,a){
+  this.name = n;
+  this.arms = a;
+
+  this.slap = function(){
+    for (var a=0;a<this.arms;a++){
+      console.log("SLAP");
+    }
+  };
+}
+
+//Write your 3 new octocat objects below here.
+var cath = new Octocat("Hans",9);
+var catg = new Octocat("Greg",8);
+var catr = new Octocat("Georing",7);
